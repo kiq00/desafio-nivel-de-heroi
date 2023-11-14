4da18120298834f82d169655947ec0ab2f29b67b
kiq00 <caique.silva2205@gmail.com>
2023-11-13 21:26:01 -0300

    desafio finalizado

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-//criando o projeto
+let nome = "Caique"
+let experiencia = 11000
+let nivel
+
+if(experiencia <= 1000){
+    nivel = "Ferro"
+}
+else if (experiencia >1000 && experiencia <=2000){
+    nivel = "Bronze"
+}
+else if(experiencia >2000 && experiencia <=6000){
+    nivel = "Prata"
+}
+else if(experiencia >6000 && experiencia <=7000){
+    nivel = "Ouro"
+}
+else if(experiencia >7000 && experiencia <=8000){
+    nivel = "Platina"
+}
+else if(experiencia >8000 && experiencia <=9000){
+    nivel = "Ascendente"
+}
+else if(experiencia >9000 && experiencia <=10000){
+    nivel = "Pimortal"
+}
+else if(experiencia >10000){
+    nivel = "Radiante"
+}
+
+console.log("O herói de nome " + nome + " está no nivel de " + nivel)
